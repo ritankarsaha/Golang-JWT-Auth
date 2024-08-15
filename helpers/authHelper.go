c8d88665c480f87dad11ca119f76445abcad3d35
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-16 03:09:15 +0530

    refactor for auth helper

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,35 +1,31 @@
 package helpers
 
-import(
+import (
 	"errors"
 	"github.com/gin-gonic/gin"
 )
 
-//checking the user type before any fucntion is done primarily
-
-func CheckUserType(c *gin.Context, roll string) (err error) {
-
-	//checking the user type of the user over here.
-
-	err = nil
+// CheckUserType checks the user type before any function is executed.
+func CheckUserType(c *gin.Context, roll string) error {
 	userType := c.GetString("user_type")
-	if userType!=roll {
-		err = errors.New("user type of the user cannot be matched by the databsase")
+	if userType != roll {
+		return errors.New("user type of the user cannot be matched by the database")
 	}
-	return err
-
-
+	return nil
 }
 
-func MathUserTypeToUid(c *gin.Context , userId string) (err error){
+// MathUserTypeToUid matches user type to UID and checks for consistency.
+func MathUserTypeToUid(c *gin.Context, userId string) error {
 	userType := c.GetString("user_type")
 	id := c.GetString("uid")
-	err = nil
-	if userType == "USER" && id!=userId{
+
+	if userType == "USER" && id != userId {
 		return errors.New("user id or uid cannot be properly matched for the user")
 	}
-	err = CheckUserType(c,userType)
-	if err !=nil{
+
+	err := CheckUserType(c, userType)
+	if err != nil {
 		return err
 	}
+	return nil
 }
