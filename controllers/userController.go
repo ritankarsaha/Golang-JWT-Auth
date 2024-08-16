9d4c302fff704b0e953069632e6abf1e730cd54e
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-17 03:09:08 +0530

    getting user controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -7,18 +7,15 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"github.com/ritankarsaha/Golang-JWT-Auth/database"
 	helper "github.com/ritankarsaha/Golang-JWT-Auth/helpers"
 	"github.com/ritankarsaha/Golang-JWT-Auth/models"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
-var validate = validator.New()
 
 func HashPassword(password string) string {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -27,4 +24,38 @@ func HashPassword(password string) string {
 		return ""
 	}
 	return string(bytes)
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+
+		if err := helper.MathUserTypeToUid(ctx, userId); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		var user models.User
+		err := userCollection.FindOne(c, bson.M{"user_id": userId}).Decode(&user)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": "User not found",
+				})
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": err.Error(),
+				})
+			}
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"user": user,
+		})
+	}
 }
