d210cc1d96caa256dc892285191c0cee091c7e67
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-16 21:47:17 +0530

    reafctor initial

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,23 +1,30 @@
 package controllers
 
 import (
-	 "context"
-	 "github.com/ritankarsaha/Golang-JWT-Auth/database"
-	 helper "github.com/ritankarsaha/Golang-JWT-Auth/helpers"
-	 "github.com/ritankarsaha/Golang-JWT-Auth/models"
-	 "log"
-	 "net/http"
-	 "time"
-	 "github.com/gin-gonic/gin"
-	 "github.com/go-playground/validator/v10"
-	 "golang.org/x/crypto/bcrypt"
-	 "go.mongodb.org/mongo-driver/bson"
-	 "go.mongodb.org/mongo-driver/bson/primitive"
-	 "go.mongodb.org/mongo-driver/mongo"
+	"context"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/ritankarsaha/Golang-JWT-Auth/database"
+	helper "github.com/ritankarsaha/Golang-JWT-Auth/helpers"
+	"github.com/ritankarsaha/Golang-JWT-Auth/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var userCollection = *&mongo.Collection = database.OpenCollection(database.Client,"user")
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
 
-func HashPassowrd()
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return string(bytes)
+}
